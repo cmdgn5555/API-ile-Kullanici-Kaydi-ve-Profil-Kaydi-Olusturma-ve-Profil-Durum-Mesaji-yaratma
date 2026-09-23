dedcb33d0da0c6147e64cfcd68be7d4c36b73026
cmdgn5555 <161839992+cmdgn5555@users.noreply.github.com>
2026-09-23 22:31:56 +0300

    Update settings.py

--- api-ile-kullanici-kayit-ve-profil-islemleri/core/core/settings.py
+++ api-ile-kullanici-kayit-ve-profil-islemleri/core/core/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^=ikrts&fj$!2dpbnvnxof)q3t_oy5#m*&!f4rnd9ud73!123(*#p'
+SECRET_KEY = 'django-insecure-^=ikrts&fj$!2dpbnvnxof)q3t_oy5#m*&!f4rnd9ud73!(*#p'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
